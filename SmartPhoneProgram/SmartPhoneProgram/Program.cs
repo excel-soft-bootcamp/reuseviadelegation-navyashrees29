eaacff2bf30434c8d17b9ecd25f2d155a698b220
navyashrees29 <navya9529@gmail.com>
2021-07-03 11:49:22 +0530

    upload

---
+++ SmartPhoneProgram/SmartPhoneProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPhoneProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera camera = new Camera();
+            GPS gps = new GPS();
+            Phone phone = new Phone();
+
+            SmartPhone smartPhone = new SmartPhone();
+            smartPhone.SetCamera(camera);
+            smartPhone.SetGPS(gps);
+            smartPhone.SetPhone(phone);
+
+            Photographer photographer = new Photographer();
+    //        photographer.Capture(smartPhone);
+            photographer.Capture(camera);
+
+            Traveller traveller = new Traveller();
+   //         traveller.Hike(smartPhone);
+            traveller.Hike(gps);
+
+            Person person = new Person();
+      //      person.Communicate(smartPhone);
+            person.Communicate(phone);
+        }
+    }
+}
---
+++ SmartPhoneProgram/SmartPhoneProgram/SmartPhone.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPhoneProgram
+{
+    class SmartPhone : ICamera,IGPS,IPhone
+    {
+        ICamera _camera;
+        IGPS _gps;
+        IPhone _phone;
+
+        public void SetCamera(ICamera camera)
+        {
+            this._camera = camera;
+        }
+        public void SetGPS(IGPS gps)
+        {
+            this._gps = gps;
+        }
+        public void SetPhone(IPhone phone)
+        {
+            this._phone = phone;
+        }
+
+        public void Click()
+        {
+            this._camera.Click();
+        }
+        public void Navigate()
+        {
+            this._gps.Navigate();
+        }
+        public void Hangup()
+        {
+            this._phone.Hangup();
+        }
+        public void Release()
+        {
+            this._phone.Release();
+        }
+
+    }
+}
